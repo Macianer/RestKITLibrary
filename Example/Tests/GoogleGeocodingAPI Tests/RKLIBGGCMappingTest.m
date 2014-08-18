323129b941b186011741325138b1959e6970efc8
ronnymeissner <owner@meissner-ronny.de>
2014-08-18 21:54:32 +0200

    initial Unit Test

---
+++ Example/Tests/GoogleGeocodingAPI Tests/RKLIBGGCMappingTest.m
@@ -0,0 +1,53 @@
+//
+//  RKLIBGGCMappingTest.m
+//  RestKITLibrary
+//
+//  Created by Ronny Meissner on 18/08/14.
+//  Copyright (c) 2014 ronnymeissner. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <RestKit/Testing.h>
+#import <RestKITLibrary/RKLIBGGC.h>
+
+@interface RKLIBGGCMappingTest : XCTestCase
+
+@end
+
+@implementation RKLIBGGCMappingTest
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    NSBundle *testBundle = [NSBundle mainBundle];
+  
+        [RKTestFixture setFixtureBundle:testBundle];
+
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+-(RKLIBGGCResult *) testObject1
+{
+    RKLIBGGCResult *object = [RKLIBGGCResult new];
+    
+    return object;
+}
+
+- (void)testFile1
+{
+    id json = [RKTestFixture parsedObjectWithContentsOfFixture:@"ggc_file1.json"];
+    
+    RKMappingTest *mappingTest = [RKMappingTest testForMapping:[RKLIBGGCMappingHelper resultMapping] sourceObject:json destinationObject:nil];
+    
+    [mappingTest addExpectation:[RKPropertyMappingTestExpectation expectationWithSourceKeyPath:@"status" destinationKeyPath:@"status"]];
+    XCTAssertTrue([mappingTest evaluate], @"");
+}
+
+@end
