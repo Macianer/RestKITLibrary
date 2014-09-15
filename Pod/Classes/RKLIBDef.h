1c511e5d43593ac35231ad19334c16d1f47fca22
ronnymeissner <owner@meissner-ronny.de>
2014-09-15 20:25:11 +0200

    update global vars

--- Pod/Classes/RKLIBDef.h
+++ Pod/Classes/RKLIBDef.h
@@ -9,6 +9,33 @@
 #ifndef Pods_RKLIBDef_h
 #define Pods_RKLIBDef_h
 
+static NSString * const kTypes = @"types";
+static NSString * const kGeocode = @"geocode";
+static NSString * const kInput = @"input";
+static NSString * const kEstablishment = @"establishment";
+static NSString * const kLanguage = @"language";
+// no errors occurred
+static NSString * const kStatusCodeOK = @"OK";
+// successful but returned no results
+static NSString * const kStatusCodeZeroResults = @"ZERO_RESULTS";
+// you are over your quota.
+static NSString * const kStatusCodeOverQueryLimit = @"OVER_QUERY_LIMIT";
+// request was denied.
+static NSString * const kStatusCodeRequestDenied = @"REQUEST_DENIED";
+// a query parameter is missing
+static NSString * const kStatusCodeInvalidRequest = @"INVALID_REQUEST";
+// unknown server error. try again.
+static NSString * const kStatusCodeUnknownError = @"UNKNOWN_ERROR";
 
+static NSString * const kJson = @"json";
+static NSString * const kXml = @"xml";
 
+static NSString * const kAddress = @"address";
+static NSString * const kSensor = @"sensor";
+static NSString * const kTrue = @"true";
+
+static NSString * const kLongTitleKey = @"long_title_key";
+static NSString * const kURLKey = @"url_title_key";
+static NSString * const kActionKey = @"action_key";
+static NSString * const kObjectManager = @"object_manager_key";
 #endif
