3ba0a47f7a4587bcfaaa68c0a33eb0d41bed42cb
Ronny Meißner <the_macianer@gmx.de>
2014-12-06 13:55:51 +0100

    add comments

--- Pod/Classes/GoogleGeocodingAPI/RKLIBGGCAPIManager.m
+++ Pod/Classes/GoogleGeocodingAPI/RKLIBGGCAPIManager.m
@@ -13,7 +13,9 @@
 
 + (instancetype)sharedManager {
 	static RKLIBGGCAPIManager *sharedMapper = nil;
+
 	static dispatch_once_t onceToken;
+
 	dispatch_once(&onceToken, ^{
 	    sharedMapper = [[RKLIBGGCAPIManager alloc] init];
 	});
@@ -24,7 +26,9 @@
  *   @return A RKObjectManager for Google Geocoding API.
  */
 - (RKObjectManager *)objectManager {
+	// check if object manager already exists
 	if (!_objectManager) {
+		// create one
 		[self _initObjectManager];
 	}
 	return _objectManager;
@@ -34,13 +38,19 @@
  *  @brief Configure a RKObjectManager for Google Geocoding requests.
  */
 - (void)_initObjectManager {
+	// create object manager
 	RKObjectManager *objectManager = [[RKObjectManager alloc] initWithHTTPClient:[AFHTTPClient clientWithBaseURL:[NSURL URLWithString:kGGCAPIUrl]]];
 
+	// set json accept header
 	[objectManager setAcceptHeaderWithMIMEType:RKMIMETypeJSON];
 
-
+	// define descriptor for RKLIBGGCResponse objects
 	RKResponseDescriptor *res = [RKResponseDescriptor responseDescriptorWithMapping:[RKLIBGGCMappingHelper responseMapping] method:RKRequestMethodGET pathPattern:kJson keyPath:nil statusCodes:[NSIndexSet indexSetWithIndex:RKStatusCodeClassSuccessful]];
+
+	// register descriptor
 	[objectManager addResponseDescriptor:res];
+
+	// return object manager
 	_objectManager = objectManager;
 }
 
@@ -48,15 +58,15 @@
  *  @brief Cancel all pending Google Geocoding requests.
  */
 - (void)cancelPendingRequests {
-	// cancel all pending requests
+	// cancel all pending requests for object manager
 	[self.objectManager cancelAllObjectRequestOperationsWithMethod:RKRequestMethodGET matchingPathPattern:kJson];
 }
 
 /*!
- *  @brief
+ *  @brief Start an request on Google Geocoding API with multiple parameters.
  *  @note  address or components is required parameter.
  *  @param address    An Address for geocoding.
- *  @param components A component filter
+ *  @param components A component filter.
  *  @param bounds     The bounding box d . "This parameter will only influence, not fully restrict, results from the geocoder" (see https://developers.google.com/maps/documentation/geocoding/).
  *  @param key        //
  *  @param language   //
@@ -72,6 +82,8 @@
                     region:(NSString *)region
                    success:(void (^)(RKObjectRequestOperation *operation, RKLIBGGCResponse *response))success
                    failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure {
+    
+    //check minimum requirement
 	NSAssert(address || components,  @"Required parameters are not set");
 
 	NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
@@ -89,10 +101,10 @@
 	if (region)
 		[dict addEntriesFromDictionary:@{ kRegion : region }];
 
-
-
+	// look for GPS sensor
 	[dict addEntriesFromDictionary:@{ kSensor : [RKLIBDeviceHelper sharedHelperInstance].hasGPSSensor ? kTrue : kFalse }];
 
+	// start request
 	[[self objectManager] getObjectsAtPath:kJson
 	                            parameters:dict success: ^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
 	    if ([mappingResult.firstObject isKindOfClass:[RKLIBGGCResponse class]]) {
